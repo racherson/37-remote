85a56a8f1c4dc0cf5c4f8aa9ca9543d0afbb0e98
Rachel Anderson <rachelanderson2020@u.northwestern.edu>
2019-11-08 00:03:54 +0000

    initial commit 7

---
+++ Deliverables/7/7.1/player_wrapper.py
@@ -0,0 +1,28 @@
+from player import Player1, Player2, Player
+from helpers import *
+
+
+class Player_Wrapper:
+	def __init__(self):
+		self.player = None
+
+	def register(self):
+		self.player = Player()
+		return self.player.get_name()
+
+	def receive_stones(self, stone):
+		self.player.set_color(stone)
+
+	def set_name(self, name):
+		self.player.set_name(name)
+
+	def get_name(self):
+		return self.player.get_name()
+
+	def get_color(self):
+		return self.player.get_color()
+
+	def make_a_move(self, boards):
+		if self.player.get_color() == EMPTY:
+			raise Exception("Player did not receive stones yet")
+		return self.player.make_a_move(boards)
---
+++ Deliverables/7/7.1/testdriver.py
@@ -0,0 +1,55 @@
+import sys
+import json
+from json import JSONDecoder
+import re
+from ref_wrapper import Ref_Wrapper
+from helpers import *
+
+REF_WRAP = Ref_Wrapper()
+NOT_WHITESPACE = re.compile(r'[^\s]')
+
+
+def point_to_string(point):
+    return str(point[1]+1) + "-" + str(point[0]+1)
+
+
+def decode_stacked(document, pos=0, decoder=JSONDecoder()):
+    while True:
+        match = NOT_WHITESPACE.search(document, pos)
+        if not match:
+            return
+        pos = match.start()
+
+        try:
+            obj, pos = decoder.raw_decode(document, pos)
+        except:
+            raise Exception("Can't parse")
+        yield obj
+
+
+s = ""
+for line in sys.stdin:
+    s += line
+
+ls = []
+counter = 0
+players = []
+most_recent_boards = [EMPTY_BOARD]
+for line in decode_stacked(s):
+    if counter == 0:
+        players.append(line)
+    elif counter == 1:
+        players.append(line)
+        color1, color2 = REF_WRAP.set_players(players[0], players[1])
+        ls.append(color1)
+        ls.append(color2)
+    else:
+        ls.append(most_recent_boards)
+        new_boards = REF_WRAP.make_action(line)
+        most_recent_boards = new_boards
+        if isinstance(new_boards[0], str):
+            ls.append(new_boards)
+            break
+    counter += 1
+
+print(json.dumps(ls, separators=(',', ':')))
