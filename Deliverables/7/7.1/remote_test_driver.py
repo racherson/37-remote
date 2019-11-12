affa01910cf795bf5c1378b22635c41ad782c314
Rachel Anderson <rachelanderson2020@u.northwestern.edu>
2019-11-12 06:05:47 +0000

    try/except for exceptions

--- Deliverables/7/7.1/remote_test_driver.py
+++ Deliverables/7/7.1/remote_test_driver.py
@@ -3,6 +3,8 @@ import json
 import pickle
 from player_wrapper import Player_Wrapper
 
+GONE_CRAZY = "GO has gone crazy!"
+
 
 def get_config():
     with open('go.config') as config_file:
@@ -12,18 +14,29 @@ def get_config():
 
 def get_response(request):
     if request[0] == "register":
-        return PLAYER_WRAP.register()
-    if request[0] == "receive-stones":
-        return PLAYER_WRAP.receive_stones(request[1])
-    if request[0] == "make-a-move":
-        return PLAYER_WRAP.make_a_move(request[1])
-    raise Exception("Invalid request")
+        try:
+            result = PLAYER_WRAP.register()
+        except:
+            result = GONE_CRAZY
+    elif request[0] == "receive-stones":
+        try:
+            result = PLAYER_WRAP.receive_stones(request[1])
+        except:
+            result = GONE_CRAZY
+    elif request[0] == "make-a-move":
+        try:
+            result = PLAYER_WRAP.make_a_move(request[1])
+        except:
+            result = GONE_CRAZY
+    else:
+        result = GONE_CRAZY
+    return result
 
 
 def try_to_connect(config):
     try:
         sock.connect((config["IP"], config["port"]))
-    except Exception:
+    except:
         try_to_connect(config)
 
 
