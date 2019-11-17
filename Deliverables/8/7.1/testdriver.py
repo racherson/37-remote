47576f07f3a00f7c86779ee3d79017d0c43c7a5a
Sarah O'Brien <sarahobrien2020@u.northwestern.edu>
2019-11-17 16:32:24 -0600

    copied over to 8

---
+++ Deliverables/8/7.1/testdriver.py
@@ -0,0 +1,96 @@
+import sys
+import json
+from json import JSONDecoder, JSONDecodeError
+import re
+import remote_player_wrapper
+
+player_wrap = remote_player_wrapper.RemotePlayerWrapper()
+NOT_WHITESPACE = re.compile(r'[^\s]')
+GONE_CRAZY = "GO has gone crazy!"
+
+
+def point_to_string(point):
+    return str(point[1]+1) + "-" + str(point[0]+1)
+
+
+def check_for_crazy(response):
+    if response == "GO has gone crazy!":
+        return True
+    return False
+
+
+def decode_stacked(document, pos=0, decoder=JSONDecoder()):
+    while True:
+        match = NOT_WHITESPACE.search(document, pos)
+        if not match:
+            return
+        pos = match.start()
+
+        try:
+            obj, pos = decoder.raw_decode(document, pos)
+        except JSONDecodeError:
+            raise Exception("Can't parse")
+        yield obj
+
+
+s = ""
+for line in sys.stdin:
+    s += line
+
+# for line in decode_stacked(s):
+#     print(line)
+# another comment
+
+ls = []
+
+for line in decode_stacked(s):
+    if len(line) == 1:
+        if line[0] == "register":
+            try:
+                response = player_wrap.register()
+                ls.append(response)
+            # continue
+            except:
+                response = GONE_CRAZY
+                ls.append(response)
+        else:
+            response = GONE_CRAZY
+            ls.append(response)
+        if check_for_crazy(response):
+            break
+
+    elif len(line) == 2:
+        if line[0] == "receive-stones":
+            try:
+                response = player_wrap.receive_stones(line[1])
+                if response:
+                    ls.append(response)
+            except:
+                response = GONE_CRAZY
+                ls.append(response)
+        elif line[0] == "make-a-move":
+            try:
+                response = player_wrap.make_a_move(line[1])
+                if len(response) == 2:
+                    ls.append(point_to_string(response))
+                else:
+                    ls.append(response)
+            except:
+                response = GONE_CRAZY
+                ls.append(response)
+        else:
+            response = GONE_CRAZY
+            ls.append(response)
+
+        if check_for_crazy(response):
+            break
+
+    else:
+        response = GONE_CRAZY
+        ls.append(response)
+        if check_for_crazy(response):
+            break
+
+player_wrap.close()
+player_wrap.sock.close()
+print(json.dumps(ls, separators=(',', ':')))
