335d3aa174b27fc0cd2d02564e567d288e1b8746
Rachel Anderson <rachelanderson2020@u.northwestern.edu>
2019-11-27 03:19:36 +0000

    oops

--- Deliverables/9/9.1/remote_player_wrapper.py
+++ Deliverables/9/9.1/remote_player_wrapper.py
@@ -18,7 +18,7 @@ class RemotePlayerWrapper:
         self.name = "no name"
         self.color = None
         self.accept_socket = accept_socket
-        self.receive_request()
+        # self.receive_request()
 
     def reset_for_new_game(self):
         self.register_flag = False
@@ -43,6 +43,7 @@ class RemotePlayerWrapper:
         return request
 
     def register(self):
+        print("register flag is:", self.register_flag)
         if self.register_flag:
             return GONE_CRAZY
         self.register_flag = True
