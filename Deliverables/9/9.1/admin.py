5c7ef21ee2b100db10146ca5b6fad3669ecd188c
Rachel Anderson <rachelanderson2020@u.northwestern.edu>
2019-12-05 08:08:46 +0000

    forgot to add to return statements

--- Deliverables/9/9.1/admin.py
+++ Deliverables/9/9.1/admin.py
@@ -11,18 +11,18 @@ def administrate(player1_wrap, player2_wrap, player1_name, player2_name, player_
     try:
         response = player1_wrap.register()
         if response == GONE_CRAZY:
-            return [player2_name], True
+            return [player2_name], True, player_names
         player_names[player1_name] = response
     except socket.error:
-        return [player2_name]
+        return [player2_name], True, player_names
 
     try:
         response = player2_wrap.register()
         if response == GONE_CRAZY:
-            return [player1_name], True
+            return [player1_name], True, player_names
         player_names[player2_name] = response
     except socket.error:
-        return [player1_name]
+        return [player1_name], True, player_names
 
     # give players to ref and ask it to play a game
     ref = Referee(player1_wrap, player2_wrap, player1_name, player2_name)
