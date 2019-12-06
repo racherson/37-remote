a1aed240e5dea021d0621c29423b70d26a0946e3
Sarah O'Brien <sarahobrien2020@u.northwestern.edu>
2019-12-06 12:57:38 -0600

    committing changes

---
+++ Deliverables/9/9.1/unittests.py
@@ -0,0 +1,9 @@
+import unittest
+from helpers import *
+from tournament_admin import *
+
+class Test_Tourney_Admin_Functions(unittest.TestCase):
+	def test_
+
+if __name__ == '__main__':
+    unittest.main()
