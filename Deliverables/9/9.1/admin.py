80d056c1affb16d9b2208edbffdb06711b83862a
Rachel Anderson <rachelanderson2020@u.northwestern.edu>
2019-12-09 22:16:49 +0000

    admin

--- Deliverables/9/9.1/admin.py
+++ Deliverables/9/9.1/admin.py
@@ -1,33 +1,18 @@
 from ref_wrapper import Ref_Wrapper
 from referee import Referee
-import socket
 from helpers import *
 
 
-def administrate(player1_wrap, player2_wrap, player1_name, player2_name, player_names):
+def administrate(player1_wrap, player2_wrap, player1_name, player2_name):
     player1_wrap.reset_for_new_game()
     player2_wrap.reset_for_new_game()
-    # register players
-    try:
-        response = player1_wrap.register()
-        if response == GONE_CRAZY:
-            return [player2_name], True, player_names
-        player_names[player1_name] = response
-    except socket.error:
-        return [player2_name], True, player_names
-
-    try:
-        response = player2_wrap.register()
-        if response == GONE_CRAZY:
-            return [player1_name], True, player_names
-        player_names[player2_name] = response
-    except socket.error:
-        return [player1_name], True, player_names
 
     # give players to ref and ask it to play a game
+    print("starting game between ", player1_name, "and ", player2_name)
     ref = Referee(player1_wrap, player2_wrap, player1_name, player2_name)
     REF_WRAP = Ref_Wrapper(ref)
     winner, illegal = REF_WRAP.play_game()
+    print("Winner: ", [winner[0]])
 
-    return winner, illegal, player_names
+    return winner, illegal
 
