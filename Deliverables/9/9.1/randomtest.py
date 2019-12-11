c2a5cef7dbbc0757c1b12a66959e73906e9d18c6
Rachel Anderson <rachelanderson2020@u.northwestern.edu>
2019-12-11 17:51:28 +0000

    finished random testing

--- Deliverables/9/9.1/randomtest.py
+++ Deliverables/9/9.1/randomtest.py
@@ -4,8 +4,10 @@ import random
 import admin
 import tournament_admin
 
-for i in range(100):
-	p1 = random.randint(0,1)
+
+print("----------------Testing Game Admin----------------")
+for i in range(10):
+	p1 = random.randint(0, 1)
 	if p1 == 0:
 		player1 = Player1("p1")
 	else:
@@ -13,8 +15,9 @@ for i in range(100):
 
 	p1_wrap = Player_Wrapper(player1.name)
 	p1_wrap.player = player1
+	name1 = p1_wrap.register()
 
-	p2 = random.randint(0,1)
+	p2 = random.randint(0, 1)
 	if p2 == 0:
 		player2 = Player1("p2")
 	else:
@@ -22,6 +25,29 @@ for i in range(100):
 
 	p2_wrap = Player_Wrapper(player2.name)
 	p2_wrap.player = player2
+	name2 = p2_wrap.register()
 
-	print(admin.administrate(p1_wrap,p2_wrap,"p1","p2", {"p1":"p1", "p2":"p2"}))
+	print(admin.administrate(p1_wrap, p2_wrap, name1, name2))
 
+print("----------------Testing Tournament Admin----------------")
+
+
+def reset_data():
+	tournament_admin.curr_default_player_num = 0
+	tournament_admin.players = {}
+	tournament_admin.rankings = {}
+	tournament_admin.beaten = {}
+	tournament_admin.player_list = []
+
+
+print("testing league")
+
+for i in range(10):
+	tournament_admin.main("--league", "0")
+	reset_data()
+
+print("\n")
+print("testing cup")
+for i in range(10):
+	tournament_admin.main("--cup", "0")
+	reset_data()
